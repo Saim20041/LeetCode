6e6a2955e5c987cdd34856735323ac70d1571fe7
Saim20041 <saimk2003@gmail.com>
2026-05-17 15:20:30 +0530

    Time: 2 ms (95.47%), Space: 43.6 MB (28.92%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        int n = operations.length;
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            if(operations[i].equals("+")){
+                list.add(list.get(list.size()-1)+list.get(list.size()-2));
+            }
+            else if(operations[i].equals("D")){
+                list.add(list.get(list.size()-1)*2);
+            }
+            else if(operations[i].equals("C")){
+                list.remove(list.size()-1);
+            }
+            else{
+                list.add(Integer.parseInt(operations[i]));
+            }
+        }
+        int sum = 0;
+        for(int i = 0; i < list.size();i++){
+            sum+=list.get(i);
+        }
+        return sum;
+
+    }
+}
