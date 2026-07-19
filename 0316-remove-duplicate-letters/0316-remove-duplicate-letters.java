5cb5f00b658161471bc51fe1f7ab16731e8b9bb6
Saim20041 <saimk2003@gmail.com>
2026-07-19 17:49:00 +0530

    Time: 2 ms (86.89%), Space: 43.8 MB (57.86%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -2,32 +2,26 @@ class Solution {
     public String removeDuplicateLetters(String s) {
         int freq[] = new int[26];
         boolean visited[] = new boolean[26];
-        for (int i = 0; i < s.length(); i++) {
+        StringBuilder sb = new StringBuilder();
+        Stack <Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
             freq[ch - 'a']++;
         }
-
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
+        for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
-            freq[ch - 'a']--;
-
-            if (visited[ch - 'a']) {
+            if(visited[ch - 'a'] == true){
+                freq[ch - 'a']--;
                 continue;
             }
-
-            while (!stack.isEmpty()
-                    && stack.peek() > ch
-                    && freq[stack.peek() - 'a'] > 0) {
-
+            while(!stack.isEmpty() && stack.peek() > ch && freq[stack.peek() - 'a'] > 0){
                 visited[stack.pop() - 'a'] = false;
             }
-
             stack.push(ch);
             visited[ch - 'a'] = true;
+            freq[ch - 'a']--;
         }
-        StringBuilder sb = new StringBuilder();
-        for (char ch : stack) {
+        for(char ch : stack){
             sb.append(ch);
         }
         return sb.toString();
