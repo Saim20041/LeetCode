46856ceffd2b483203fc4b3bf6f3b7534c3e762b
Saim20041 <saimk2003@gmail.com>
2026-07-24 15:25:35 +0530

    Time: 19 ms (70.42%), Space: 54.6 MB (31.33%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,32 +1,25 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        
-        int dp[][] = new int[s.length()+1][t.length()+1];
-        for(int row[] : dp){
-            Arrays.fill(row, -1);
+        int m = s.length();
+        int n = t.length();
+        int dp[][] = new int[m+1][n+1];
+        for(int i = 0; i <= m; i++){
+            dp[i][n] = 1;
         }
-        return helper(0, 0, s, t, dp);
+        for(int i = m-1; i >=0; i--){
+            for(int j = n-1; j >=0; j--){
+                if(s.charAt(i) == t.charAt(j)){
+                    int case1 = dp[i+1][j+1];
+                    int case2 = dp[i+1][j];
+                    dp[i][j] = case1+case2;
+                    
+                }
+                else{
+                    dp[i][j] = dp[i+1][j];
+                    
+                }
+            }
+        }
+        return dp[0][0];
     }
-    public int helper(int i, int j, String s, String t, int dp[][]){
-        if(j == t.length()){
-            dp[i][j] = 1;
-            return dp[i][j];
-        }
-        if(i == s.length()){
-            dp[i][j] = 0;
-            return dp[i][j];
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        if(s.charAt(i) == t.charAt(j)){
-            int case1 = helper(i+1, j+1, s, t, dp);
-            int case2 = helper(i+1, j, s, t, dp);
-            dp[i][j] = case1+case2;
-            return dp[i][j];
-        }
-        dp[i][j] = helper(i+1, j, s, t, dp);
-        return dp[i][j];
-    }
-
 }
