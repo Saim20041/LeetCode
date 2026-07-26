21d23bb4fce013df0e7710bb06c50c1da5cda964
Saim20041 <saimk2003@gmail.com>
2026-07-26 20:45:36 +0530

    Time: 19 ms (22.99%), Space: 54 MB (60.34%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,19 +1,29 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int dp[][] = new int[amount + 1][coins.length];
-        for (int j = 0; j < coins.length; j++) {
-            dp[0][j] = 1;
+        int dp[][] = new int[amount+1][coins.length];
+        for(int row[] : dp){
+            Arrays.fill(row ,-1);
         }
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                if (i >= coins[j]) {
-                    dp[i][j] += dp[i - coins[j]][j];
-                }
-                if (j > 0) {
-                    dp[i][j] += dp[i][j - 1];
-                }
-            }
+        return helper(amount, coins.length-1, coins, dp);
+    }
+    private int helper(int amount, int index, int coins[], int dp[][]){
+        if(amount == 0){
+            return 1;
         }
-        return dp[amount][coins.length - 1];
+        if(amount < 0){
+            return 0;
+        }
+        if(index < 0){
+            return 0;
+        }
+        if(dp[amount][index] != -1){
+            return dp[amount][index];
+        }
+        int case1 = helper(amount - coins[index], index, coins, dp);
+        int case2 = helper(amount, index-1, coins, dp);
+        dp[amount][index] = case1 + case2;
+        return dp[amount][index];
+        
+        
     }
 }
