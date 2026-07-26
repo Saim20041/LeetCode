1bf05042c39524f6cdd744f373d69d07d184607f
Saim20041 <saimk2003@gmail.com>
2026-07-26 20:10:27 +0530

    Time: 29 ms (16.57%), Space: 58.2 MB (6.61%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int dp[][] = new int[amount+1][coins.length+1];
+        for(int row[] : dp){
+            Arrays.fill(row ,-1);
+        }
+        return helper(amount, 0, coins, dp);
+    }
+    private int helper(int amount, int index, int coins[], int dp[][]){
+        if(amount == 0){
+            return 1;
+        }
+        if(amount < 0){
+            return 0;
+        }
+        if(index == coins.length){
+            return 0;
+        }
+        if(dp[amount][index] != -1){
+            return dp[amount][index];
+        }
+        int case1 = helper(amount - coins[index], index, coins, dp);
+        int case2 = helper(amount, index+1, coins, dp);
+        dp[amount][index] = case1 + case2;
+        return dp[amount][index];
+        
+        
+    }
+}
