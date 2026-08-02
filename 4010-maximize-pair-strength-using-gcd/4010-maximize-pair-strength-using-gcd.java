88e98b545b671387adce3bfd6dddfe25062b5aea
Saim20041 <saimk2003@gmail.com>
2026-08-02 16:30:38 +0530

    Time: 1823 ms (5.01%), Space: 46.8 MB (81.16%) - LeetHub

---
+++ 4010-maximize-pair-strength-using-gcd/4010-maximize-pair-strength-using-gcd.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long maxPairStrength(int[] nums) {
+        long max = 0;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < nums.length; j++){
+                if(i == j){
+                    continue;
+                }
+                int a = nums[i];
+                int b = nums[j];
+                while(b != 0){
+                    int temp = b;
+                    b = a % b;
+                    a = temp;
+                    
+                }
+                long curr = (1L * nums[i] * nums[j])/(a*a);
+                max = Math.max(max, curr);
+            }
+        }
+        return max;
+    }
+}
