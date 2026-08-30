49586a4678ab5572f24a0dc16e9719f6d0d615ff
Saim20041 <saimk2003@gmail.com>
2026-08-30 16:35:54 +0530

    Time: 750 ms (15.63%), Space: 53.2 MB (5.65%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int count = 0;
+        int dp[][] = new int[s.length()][s.length()];
+        for(int arr[] : dp){
+            Arrays.fill(arr, -1);
+        }
+        for(int i = 0; i < s.length(); i++){
+            count+=helper(s, i, i, dp);
+        }
+        return count;
+    }
+    public int helper(String s, int left, int right, int dp[][]){
+        if(right == s.length()){
+            return 0;
+        }
+        if(dp[left][right]!=-1){
+            return dp[left][right];
+        }
+        if(isPalindrome(s, left, right)){
+            dp[left][right] = 1 + helper(s, left, right+1, dp);
+            return dp[left][right];
+        }
+        else{
+            dp[left][right] = helper(s, left, right+1, dp);
+            return dp[left][right];
+        }
+
+
+    }
+    public boolean isPalindrome(String s, int left, int right){
+        if(left < 0 || right >= s.length()){
+            return false;
+        }
+        while(left <= right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
